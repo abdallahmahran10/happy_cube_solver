4069b028f66350ff4708e0274314745c0cbd6cb6
Abdallah Mahran <eng.abdallah.mahran@gmail.com>
2017-01-04 16:40:09 +0100

    Update Cube.java

--- src/com/task_360t/cubes/models/Cube.java
+++ src/com/task_360t/cubes/models/Cube.java
@@ -1,8 +1,8 @@
 package com.task_360t.cubes.models;
 
 import java.util.BitSet;
-import java.util.List;
 
+import com.task_360t.cubes.utilities.CONSTANTS;
 import com.task_360t.cubes.utilities.CubeLogger;
 
 public class Cube {
@@ -12,42 +12,59 @@ public class Cube {
 
 	public Cube() {
 		faces = new Piece[6];
-		for(int i=0; i<6; ++i)
+		for (int i = 0; i < 6; ++i)
 			faces[i] = new Piece();
 		currentFaceIdx = 0;
 	}
 
-	
-
-	public int createNextFace(List<Piece> pieces) {
-		
-		for (int i=0; i<pieces.size(); i++) {
-			if (matchCurrentFace(pieces.get(i))) {
-				faces[currentFaceIdx] = pieces.get(i);
-				++currentFaceIdx;
-				return i;
-			}
-		}
-		return -1;
+	public Cube(Cube cube) {
+		this.faces = new Piece[6];
+		System.arraycopy(cube.faces, 0, this.faces, 0, cube.faces.length);
+		this.currentFaceIdx = cube.currentFaceIdx;
 	}
 
-	private boolean matchCurrentFace(Piece piece) {
-		if (currentFaceIdx == 0)
+	public boolean isPieceMatch(Piece piece) {
+		switch (currentFaceIdx) {
+		case 0:
 			return true;
-		
-		return tryToMatch(piece);
-	}
-	
-	private boolean tryToMatch(Piece piece) {
-		BitSet currentEdge = getCurrentEdge();
-		logger.INFO(Long.toString(currentEdge.toLongArray()[0], 2));
-		for(int i=0; i<4; i++)
-		{
-			BitSet interstingEdge = getInterstingEdge(piece);
-			logger.INFO(Long.toString(interstingEdge.toLongArray()[0], 2));
-			if(matches(currentEdge, interstingEdge))
-				return true;
-			piece.rotateClockWise();
+		case 1:
+			return matches(faces[0].getRightEdge(), piece.getLeftEdge());
+
+		case 2:
+			return matches(faces[1].getRightEdge(), piece.getLeftEdge());
+
+		case 3:
+			return matches(faces[0].getBottomEdge(), piece.getLeftEdge())
+					&& matches(faces[1].getBottomEdge(), piece.getTopEdge())
+					&& matches(faces[2].getBottomEdge(), piece.getRightEdge())
+					&& matches(faces[0].getCornerBit(CornersBit.BR), faces[1].getCornerBit(CornersBit.BL),
+							piece.getCornerBit(CornersBit.TL))
+					&& matches(faces[2].getCornerBit(CornersBit.BL), faces[1].getCornerBit(CornersBit.BR),
+							piece.getCornerBit(CornersBit.TR));
+
+		case 4:
+			return matches(faces[0].getLeftEdge(), piece.getLeftEdge())
+					&& matches(faces[2].getRightEdge(), piece.getRightEdge())
+					&& matches(faces[3].getBottomEdge(), piece.getTopEdge())
+					&& matches(faces[0].getCornerBit(CornersBit.BL), faces[3].getCornerBit(CornersBit.BL),
+							piece.getCornerBit(CornersBit.TL))
+					&& matches(faces[2].getCornerBit(CornersBit.BR), faces[3].getCornerBit(CornersBit.BR),
+							piece.getCornerBit(CornersBit.TR));
+
+		case 5:
+			return matches(faces[0].getTopEdge(), piece.getLeftEdge())
+					&& matches(faces[2].getTopEdge(), piece.getRightEdge())
+					&& matches(faces[1].getTopEdge(), piece.getBottomEdge())
+					&& matches(faces[0].getCornerBit(CornersBit.TL), faces[4].getCornerBit(CornersBit.BL),
+							piece.getCornerBit(CornersBit.TL))
+					&& matches(faces[4].getCornerBit(CornersBit.BR), faces[2].getCornerBit(CornersBit.TR),
+							piece.getCornerBit(CornersBit.TR))
+					&& matches(faces[0].getCornerBit(CornersBit.TR), faces[1].getCornerBit(CornersBit.TL),
+							piece.getCornerBit(CornersBit.BL))
+					&& matches(faces[1].getCornerBit(CornersBit.TR), faces[2].getCornerBit(CornersBit.TL),
+							piece.getCornerBit(CornersBit.BR));
+		default:
+			break;
 		}
 		return false;
 	}
@@ -56,77 +73,35 @@ public class Cube {
 		// TODO Auto-generated method stub
 		BitSet set = (BitSet) currentEdge.clone();
 		set.or(interstingEdge);
-		int idx = set.nextClearBit(0);
-		//logger.INFO(Long.toString(set.toLongArray()[0], 2));
-		return idx == 5;
+		return set.get(1) && set.get(2) && set.get(3);
 	}
 
-	private BitSet getInterstingEdge(Piece piece) {
-		switch (currentFaceIdx) {
-		case 1:
-			return piece.getLeftEdge();
-		
-		case 2:
-			return faces[1].getLeftEdge();
-			
-		case 3:
-			return faces[1].getTopEdge();
-
-		case 4:
-			faces[3].getTopEdge();
-			break;
-		case 5:
-			faces[4].getTopEdge();
-			break;
-
-		default:
-			break;
-		}
-		return null;
+	private boolean matches(boolean a, boolean b, boolean c) {
+		return (a && !b && !c) || (!a && b && !c) || (!a && !b && c);
 	}
 
-	private BitSet getCurrentEdge() {
-		switch (currentFaceIdx) {
-		case 1:
-			return faces[0].getRightEdge();
-		
-		case 2:
-			return faces[1].getRightEdge();
-			
-		case 3:
-			return faces[1].getBottomEdge();
-
-		case 4:
-			faces[3].getBottomEdge();
-			break;
-		case 5:
-			faces[4].getBottomEdge();
-			break;
-
-		default:
-			break;
-		}
-		return null;
+	public void setNextFace(Piece piece) {
+		faces[currentFaceIdx] = piece;
+		++currentFaceIdx;
 	}
 
-	public boolean isCubeFormed()
-	{
+	public boolean isCubeFormed() {
 		return currentFaceIdx == 6;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuffer buff = new StringBuffer();
-		for(int i=0; i<5; i++)
-		{
+		for (int i = 0; i < 5; i++) {
 			buff.append(faces[0].getRowStr(i));
 			buff.append(faces[1].getRowStr(i));
 			buff.append(faces[2].getRowStr(i));
 			buff.append(System.lineSeparator());
 		}
-		buff.append(faces[3].toString("            "));
-		buff.append(faces[4].toString("            "));
-		buff.append(faces[5].toString("            "));
+		buff.append(faces[3].toString(CONSTANTS.INDENT));
+		buff.append(faces[4].toString(CONSTANTS.INDENT));
+		buff.append(faces[5].toString(CONSTANTS.INDENT));
 		return buff.toString();
 	}
+
 }
